ad5383494e90985d38046219c693665fa80503ec
gilman qazi <gilmanqazi@gmail.com>
2025-12-28 21:37:20 +0530

    Card Project

---
+++ Class7-Project/src/App.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import Card from "./components/Card.jsx";
+
+const App = () => {
+  const [userName, setUserName] = useState("");
+  const [imageURL, setImageURL] = useState("");
+  const [userRole, setUserRole] = useState("");
+  const [userdesc, setUserDesc] = useState("");
+
+  const [allUsers, setAllUsers] = useState([]);
+
+  function submitHandeler(e) {
+    e.preventDefault();
+    
+setAllUsers([...allUsers,{userName,userRole,userdesc,imageURL}])
+    setUserName("");
+    setImageURL("");
+    setUserRole("");
+    setUserDesc("");
+  }
+
+ const deleteHandler = (idx)=>{
+ const copyUser = [...allUsers]
+ copyUser.splice(idx,1)
+ setAllUsers(copyUser)
+ }
+
+  return (
+    <div className="h-full w-full bg-black text-white">
+      <form
+        className="p-10 flex gap-4 flex-col  items-center"
+        onSubmit={(e) => {
+          submitHandeler(e);
+        }}
+      >
+        <input
+          className="border-2 py-2 px-5 rounded w-fit "
+          type="text"
+          placeholder="Enter Your Name"
+          value={userName}
+          onChange={(e) => {
+            setUserName(e.target.value);
+          }}
+        />
+
+        <input
+          className="border-2 py-2 px-5 rounded "
+          type="text"
+          placeholder="Image URL"
+          value={imageURL}
+          onChange={(e) => {
+            setImageURL(e.target.value);
+          }}
+        />
+
+        <input
+          className="border-2 py-2 px-5 rounded "
+          type="text"
+          placeholder="Enter Role"
+          value={userRole}
+          onChange={(e) => {
+            setUserRole(e.target.value);
+          }}
+        />
+
+        <input
+          className="border-2 py-2 px-5 rounded "
+          type="text"
+          placeholder="Enter Description"
+          value={userdesc}
+          onChange={(e) => {
+            setUserDesc(e.target.value);
+          }}
+        />
+
+        <button className="bg-green-900 py-1 px-5 rounded active:scale-95">
+          Submit
+        </button>
+      </form>
+
+      <div className="p-2 flex gap-4 flex-wrap  items-center">
+
+   {allUsers.map(function(elem,idx){
+    return    <Card idx={idx} elem ={elem} deleteHandler = {deleteHandler}/>
+   })}
+      </div>
+    
+    </div>
+  );
+};
+
+export default App;
---
+++ Class7-Project/src/components/Card.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Card = (props) => {
+  console.log(props)
+  return (
+    <div  className=' w-[28vw] h-80 bg-white rounded-xl flex items-center flex-col gap-4 p-5 text-black'>
+    <img className='h-15 w-15 rounded-full object-center object-cover' src={props.elem.imageURL} alt="" /> 
+
+   <h1 className='text-xl font-semibold'>{props.elem.userName}</h1>
+   <h5 className='text-base font-semibold text-blue-500'>{props.elem.userRole}</h5>
+   <p className='text-sm font-medium leading-tight text-center'>{props.elem.userdesc}</p>
+   <button onClick={()=>{
+    props.deleteHandler(props.idx)
+   }} className='px-4 py-1 bg-red-600 rounded font-semibold active:scale-95'>RemoveUser</button>
+   
+ </div>
+  )
+}
+
+export default Card
